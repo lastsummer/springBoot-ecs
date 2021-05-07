e285c5bd59af578c69ac5bf03f46f6df9eaaa677
tinghung.wang[王庭歡] <tinghung.wang@tinghungwang.local>
2021-05-07 15:09:58 +0800

    add 01 exaple

---
+++ 01-hello-world-rest-api/src/main/java/com/example/springbootToECS01/SpringbootToEcs01Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootToECS01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootToEcs01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootToEcs01Application.class, args);
+	}
+
+}
---
+++ 01-hello-world-rest-api/src/test/java/com/example/springbootToECS01/SpringbootToEcs01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootToECS01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootToEcs01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
